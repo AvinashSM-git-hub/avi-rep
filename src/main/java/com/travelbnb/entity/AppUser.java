87c9cdf4e5213ca26b63b4900fd99a5fbf3ff468
Avinash <avinashsm05@gmail.com>
2024-12-18 11:59:47 +0530

    test1

---
+++ src/main/java/com/travelbnb/entity/AppUser.java
@@ -0,0 +1,17 @@
+package com.travelbnb.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "app_user")
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private java.lang.Long id;
+
+}
